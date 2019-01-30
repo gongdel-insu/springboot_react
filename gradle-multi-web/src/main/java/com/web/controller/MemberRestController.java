07637cca0bb5970d9b3fb2c944b4c0e60c45b37b
gonghojin <yolip51@naver.com>
2019-01-31 02:14:06 +0900

    Add: 멀티모듈 독립 jar, jar 테스트

---
+++ gradle-multi-web/src/main/java/com/web/controller/MemberRestController.java
@@ -0,0 +1,20 @@
+package com.web.controller;
+
+import com.core.domain.user.Member;
+import com.core.domain.user.MemberRepository;
+import com.web.service.user.MemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class MemberRestController {
+
+    private MemberService memberService;
+
+    @GetMapping("/")
+    public Member get() {
+        return new Member("gongdel", "gongdel@gmail.com");
+    }
+}
