0d72c5851f9cd8f35b2516cef78b79ee6a5692b5
gonghojin <yolip51@naver.com>
2019-02-04 19:24:53 +0900

    Test : aws codedeploy5

--- gradle-multi-web/client/src/App.tsx
+++ gradle-multi-web/client/src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.[test: aws code deploy3]
+          To get started, edit <code>src/App.tsx</code> and save to reload.[test: aws code deploy4]
         </p>
       </div>
     );
