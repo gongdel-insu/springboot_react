beea53e946e5850aa8ee249517f98ab72528c592
gonghojin <yolip51@naver.com>
2019-02-06 14:47:41 +0900

    Test : local, real 분리 작업중

--- gradle-multi-web/src/main/java/com/RestWebApplication.java
+++ gradle-multi-web/src/main/java/com/RestWebApplication.java
@@ -2,10 +2,20 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RestWebApplication {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-react/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(RestWebApplication.class, args);
+        new SpringApplicationBuilder(RestWebApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
+
 }
+
--- gradle-multi-web/src/main/java/com/web/controller/WebTestRestController.java
+++ gradle-multi-web/src/main/java/com/web/controller/WebTestRestController.java
@@ -4,17 +4,28 @@ import com.core.domain.user.Member;
 import com.core.domain.user.MemberRepository;
 import com.web.service.user.MemberService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
-public class MemberRestController {
+public class WebTestRestController {
 
     private MemberService memberService;
+    private Environment environment;
 
     @GetMapping("/test")
     public Member get() {
         return new Member("gongdel", "gongdel@gmail.com");
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
